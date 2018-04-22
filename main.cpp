510d173b38916021e6065161f6b2e056b7b77497
Sewon Park <33444282+Sangjeona@users.noreply.github.com>
2018-04-22 23:07:03 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,3 @@
-//
-//  main.cpp
-//  OBGG
-//
-//  Created by Sewon Park on 22/04/2018.
-//  Copyright © 2018 Sewon Park. All rights reserved.
-//
-
 #include <iostream>
 
 int main(int argc, const char * argv[]) {
